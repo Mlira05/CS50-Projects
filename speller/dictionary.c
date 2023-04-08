c5386f3e0a7665c55470aa75c06d864f3ad149d2
Matheus Lira <108106843+Mlira05@users.noreply.github.com>
2023-04-08 17:25:25 -0300

    Problem Set 5: Speller
    A program that spell-checks a file, using a hash table.

---
+++ speller/dictionary.c
@@ -0,0 +1,111 @@
+// Implements a dictionary's functionality
+
+#include <ctype.h>
+#include <stdbool.h>
+#include <stdio.h>
+#include <string.h>
+#include <strings.h>
+#include <stdlib.h>
+
+#include "dictionary.h"
+
+// Represents a node in a hash table
+typedef struct node
+{
+    char word[LENGTH + 1];
+    struct node *next;
+}
+node;
+
+// Number of buckets in hash table
+const unsigned int N = 26;
+
+// Hash table
+node *table[N];
+
+// Counter for the number of words in the dictionary
+unsigned int word_count = 0;
+
+// Returns true if word is in dictionary, else false
+bool check(const char *word)
+{
+    node *cursor = table[hash(word)];
+    while (cursor != NULL)
+    {
+        if (strcasecmp(cursor->word, word) == 0)
+        {
+            return true;
+        }
+        cursor = cursor->next;
+    }
+    return false;
+}
+
+// Hashes word to a number
+unsigned int hash(const char *word)
+{
+    //Hashes word to a number between 0 and 25, inclusive, based on its first letter
+    return tolower(word[0]) - 'a';
+}
+
+// Loads dictionary into memory, returning true if successful, else false
+bool load(const char *dictionary)
+{
+    // Open dictionary file
+    FILE *file = fopen(dictionary, "r");
+    if (file == NULL)
+    {
+        return false;
+    }
+
+    // Buffer for a word
+    char word[LENGTH + 1];
+
+    // Insert words into hash table
+    while (fscanf(file, "%s", word) != EOF)
+    {
+        node *n = malloc(sizeof(node));
+        if (n == NULL)
+        {
+            unload();
+            return false;
+        }
+
+        // Create New Node
+        strcpy(n->word, word);
+
+        // Hash Word and insert the node into the hash table
+        n->next = table[hash(word)];
+        table[hash(word)] = n;
+
+        // Count words for size function
+        word_count++;
+    }
+
+    // Close dictionary file
+    fclose(file);
+
+    return true;
+}
+
+// Returns number of words in dictionary if loaded, else 0 if not yet loaded
+unsigned int size(void)
+{
+    return word_count;
+}
+
+// Unloads dictionary from memory, returning true if successful, else false
+bool unload(void)
+{
+    for (int i = 0; i < N; i++)
+    {
+        node *cursor = table[i];
+        while (cursor != NULL)
+        {
+            node *temp = cursor;
+            cursor = cursor->next;
+            free(temp);
+        }
+    }
+    return true;
+}
